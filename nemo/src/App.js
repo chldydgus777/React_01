4fced53338f70fc8abb0a0714060eb5fb9062b5a
chldydgus777 <chldydgus777@naver.com>
2021-03-23 15:24:06 +0900

    React001

--- nemo/src/App.js
+++ nemo/src/App.js
@@ -1,47 +1,37 @@
 import React from "react";
+import Nemo from "./Nemo";
 
 class App extends React.Component{
   constructor(props){
     super(props);
 
     this.state ={
-        count: 3,
     };
+    
+    this.div =React.createRef();
   }
 
-  addNemo = () => {
-    this.setState({count: this.state.count +1})
-  console.log("addNemo");
-  } 
-  removeNemo = () => { 
-    if (this.state.count > 0 ) {   
-    this.setState({count: this.state.count -1})
-  }else {
-    window.alert("네모가 없어요!");
+  hoverEvent = (e) => {
+    console.log(e);
+    console.log(e.target)
+
+    e.target.style.background = "#eee";
+  }
+
+  componentDidMount(){
+    this.div.current.addEventListener("mouseover", this.hoverEvent)
 
   }
-  console.log("removeNemo");
+
+  componentWillUnmount(){
+    this.div.current.removeEventListener("mouseover", this.hoverEvent)
   }
 
   render(){
-    const nemo_count = Array.from({length: this.state.count}, (v, i)=>(i));
-    console.log(nemo_count);
+    
     return (
-      <div className="App">
-        {nemo_count.map((num, idx)=>{
-          return (
-            <div key={idx} style={{
-              width:'150px',
-              height: '150px',
-              backgroundColor: '#eee',
-              margin: '10px'
-            }}>
-              nemo
-            </div>
-          );
-        })}
-        <button onClick={this.addNemo}>하나 추가 !</button>
-        <button onClick={this.removeNemo}>하나 빼기 !</button>
+      <div className="App" ref={this.div}>
+        <Nemo/>
     </div>
     )
 
